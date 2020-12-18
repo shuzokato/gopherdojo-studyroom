641e3ee407f57c96fcc04c00cff3e8565ed51927
Shuzo Kato <33191593+shuzokato@users.noreply.github.com>
2020-12-19 01:04:53 +0900

    add functions

--- kadai1/shuzokato/convert/convert.go
+++ kadai1/shuzokato/convert/convert.go
@@ -3,23 +3,59 @@
 package convert
 
 import (
+	"bufio"
+	"fmt"
 	"image/jpeg"
 	"image/png"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
+// Convert image
 func Convert() {
-	file, err := os.Open("./src/gopher.jpg")
-	if err != nil {
-		panic(err)
+	fpaths := getFiles(".")
+	fmt.Println(fpaths)
+	for _, fpath := range fpaths {
+		buf, file := readImage(fpath)
+		defer file.Close()
+		img, err := jpeg.Decode(buf)
+		var pngFileName string
+		pngFileName = fmt.Sprint(fpath[:len(fpath)-len(filepath.Ext(fpath))], ".png")
+		out, err := os.Create(pngFileName)
+		if err != nil {
+			panic(err)
+		}
+		defer out.Close()
+		png.Encode(out, img)
 	}
-	defer file.Close()
-	img, err := jpeg.Decode(file)
+}
 
-	out, err := os.Create("./src/gopher.png")
+// read input image
+func readImage(fpath string) (*bufio.Reader, *os.File) {
+	file, err := os.Open(fpath)
 	if err != nil {
 		panic(err)
 	}
-	defer out.Close()
-	png.Encode(out, img)
+	buf := bufio.NewReader(file)
+	return buf, file
+}
+
+func getFiles(dir string) []string {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+	var fpaths []string
+	for _, file := range files {
+		fpath := filepath.Join(dir, file.Name())
+		if file.IsDir() {
+			fpaths = append(fpaths, getFiles(fpath)...)
+			continue
+		}
+		if filepath.Ext(file.Name()) == ".jpg" {
+			fpaths = append(fpaths, fpath)
+		}
+	}
+	return fpaths
 }
